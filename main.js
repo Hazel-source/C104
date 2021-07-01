646065c7af44853a98cc7a0ce1ca0802dc620c62
Hazel-source <82257530+Hazel-source@users.noreply.github.com>
2021-07-01 18:59:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+Webcam.set({
+ width:350,
+ height:300,
+image_format:'png',
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+console.log("ml5 version", ml5.version);
